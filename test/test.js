db26e6394ac57f96e9f661286fd57e0ea76746c9
Andrew Kelley <superjoe30@gmail.com>
2012-10-11 23:08:34 -0400

    add test for moving folders

--- test/test.js
+++ test/test.js
@@ -5,6 +5,22 @@ proxyquire = require('proxyquire');
 fs = require('fs');
 
 describe("mv", function() {
+  var mock_fs, mocked_mv;
+
+  // makes fs.rename return cross-device error.
+  mock_fs = {};
+  mock_fs.createReadStream = fs.createReadStream;
+  mock_fs.createWriteStream = fs.createWriteStream;
+  mock_fs.unlink = fs.unlink;
+  mock_fs.rename = function(src, dest, cb) {
+    setTimeout(function() {
+      var err;
+      err = new Error();
+      err.code = 'EXDEV';
+      cb(err);
+    }, 10);
+  };
+
   it("should rename a file on the same device", function (done) {
     var mv;
 
@@ -22,20 +38,7 @@ describe("mv", function() {
   });
 
   it("should work across devices", function (done) {
-    var mv, mock_fs;
-
-    mock_fs = {};
-    mock_fs.createReadStream = fs.createReadStream;
-    mock_fs.createWriteStream = fs.createWriteStream;
-    mock_fs.unlink = fs.unlink;
-    mock_fs.rename = function(src, dest, cb) {
-      setTimeout(function() {
-        var err;
-        err = new Error();
-        err.code = 'EXDEV';
-        cb(err);
-      }, 10);
-    };
+    var mv;
 
     mv = proxyquire.resolve('../index', __dirname, {fs: mock_fs});
     mv("test/a-file", "test/a-file-dest", function (err) {
@@ -45,6 +48,22 @@ describe("mv", function() {
         assert.strictEqual(contents, "sonic the hedgehog\n");
         // move it back
         mv("test/a-file-dest", "test/a-file", done);
+      });
+    });
+  });
+
+  it("should move folders", function (done) {
+    var mv;
+
+    mv = proxyquire.resolve('../index', __dirname, {});
+
+    mv("test/a-folder", "test/a-folder-dest", function (err) {
+      assert.ifError(err);
+      fs.readFile("test/a-folder-dest/another-file", 'utf8', function (err, contents) {
+        assert.ifError(err);
+        assert.strictEqual(contents, "tails\n");
+        // move it back
+        mv("test/a-folder-dest", "test/a-folder", done);
       });
     });
   });
